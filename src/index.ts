56018eb66c047407322accadeb270149b7503037
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-18 02:27:58 +0200

    add async test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import arrays from "./utilities/arrays";
 import numbers from "./utilities/numbers";
 import strings from "./utilities/strings";
-
+/** Use Axios to get data from restcountries api */
+import axios from 'axios';
 // const myFun = (num: number): number => {
 //   return num * num;
 // };
@@ -20,3 +21,66 @@ console.log(numbers.sum(arrSum, myNum));
 console.log(strings.capitalize("the quick brown fox"));
 console.log(numbers.multiply(five, 8));
 console.log(arrays.lgNum(mixArr));
+
+
+
+
+
+
+/** Use the free API https://restcountries.eu/
+ * You will use the following endpoints
+ * https://restcountries.eu/rest/v2/name/{name} for countries by name
+ * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
+ */
+
+/** Create getCountry Function here */
+async function getCountry(name: string) {
+  const getApi = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
+  const data = getApi.data[0];
+  console.log(`
+    capital: ${data.capital},
+    region: ${data.region},
+    numericCode: ${data.numericCode}
+  `);
+
+  return {
+    capital: data.capital,
+    region: data.region,
+    numericCode: data.numericCode
+  };
+}
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+/** Create getRegionCapitals function here */
+async function getRegionCapitals(regionalbloc: string) {
+  const countryEndpoints = await getRegionCountries(regionalbloc);
+  const promises = countryEndpoints.map((endpoint: any) =>
+    axios(`https://restcountries.eu/rest/v2/name/${endpoint}`
+    ));
+  const arr = await Promise.all(promises);
+  const capitals = [];
+  for (let i = 0; i < arr.length; i++) {
+    capitals.push(arr[i].data[0].capital);
+  }
+  return capitals;
+}
+
+
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals
+};
+
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,6 +2,8 @@
 import arrays from '../utilities/arrays';
 import numbers from '../utilities/numbers';
 import strings from '../utilities/strings';
+import countries from "../index";
+
 
 // it('expect myFun(5) to equal 25', () => {
 //     expect(myFun(5)).toEqual(25);
@@ -36,4 +38,33 @@ it('should not contain the third index', () => {
 
 it('should not have a large number and be falsy', () => {
     expect(arrays.lgNum(wordArr)).toBeFalsy();
+});
+
+
+
+
+it("should get basic data on the country canada", async () => {
+    const data = await countries.getCountry('canada');
+    expect(data).toEqual({
+        capital: 'Ottawa',
+        region: 'Americas',
+        numericCode: '124'
+    });
+});
+
+
+
+/** Add test for getRegionCountries function here */
+it("should get the countries in the region NAFTA", async () => {
+    const data = await countries.getRegionCountries('nafta');
+    expect(data).toEqual([
+        'Canada', 'Mexico', 'United States of America'
+    ]);
+});
+
+it("should get capitals of NAFTA countries", async () => {
+    const data = await countries.getRegionCapitals('nafta');
+    expect(data).toEqual([
+        'Ottawa', 'Mexico City', 'Washington, D.C.'
+    ]);
 });
